fb1fed9b41e790cd877301a6d544c3615937bf8e
abhi <abhikm911@gmail.com>
2023-11-19 21:00:05 +0530

    fixed css

---
+++ src/components/DummyData.js
@@ -0,0 +1,34 @@
+const employeeData = [
+  {
+    id: 1,
+    name: "Arjun",
+    dob: "16-11-2002",
+    role: "Software Engineer",
+  },
+  {
+    id: 2,
+    name: "Mahesh",
+    dob: "19-01-2001",
+    role: "Product Manager",
+  },
+  {
+    id: 3,
+    name: "Abhishek",
+    dob: "11-09-2002",
+    role: "Backend Developer",
+  },
+  {
+    id: 4,
+    name: "Harshit",
+    dob: "01-01-2003",
+    role: "Data Analyst",
+  },
+  {
+    id: 5,
+    name: "Abhigyan",
+    dob: "01-01-2003",
+    role: "Dev Ops",
+  },
+];
+
+export default employeeData;
